2f8da29e65275b60ae14686563294571a49bec92
Downueaa_ <162528777+Downuea2545@users.noreply.github.com>
2024-09-10 21:02:32 +0700

    Clone

---
+++ WebApp_Mini_Project/Controllers/PostController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using System.Diagnostics;
+using WebApp_Mini_Project.Models;
+
+namespace WebApp_Mini_Project.Controllers
+{
+    public class PostController : Controller
+    {
+        // ตัวอย่างข้อมูลจำลอง
+        private static List<Post> posts = new List<Post>
+        {
+            new Post("AJMCJ19", 5, "หาสาวเสียงน่ารักๆ", "Valorant"),
+            new Post("1748200", 3, "หาควายให้แบกขอแรงค์ iron", "Rov"),
+            new Post("xxxxxxx", 2, "Gold - Plat ชิลๆ ไม่ซี", "Valorant"),
+        };
+
+        // GET: Post
+        public ActionResult Index()
+        {
+            return View(posts);
+        }
+
+        // GET: Post/Details/5
+        public ActionResult Details(int id)
+        {
+            var post = posts[id];
+            return View(post);
+        }
+    }
+}
---
+++ WebApp_Mini_Project/Models/Post.cs
@@ -0,0 +1,29 @@
+﻿namespace WebApp_Mini_Project.Models
+{
+    public class Post
+    {
+        public string id_room { get; set; }
+        public int person_count { get; set; }
+        public string details { get; set; }
+        public string game_name { get; set; }
+
+        public Post(string Id_room, int Person_count, string Details, string Game_name)
+        {
+            id_room = Id_room;
+            person_count = Person_count;
+            details = Details;
+            game_name = Game_name;
+        }
+
+        public void Displayinfo()
+        {
+            Console.WriteLine($"Id room   : {this.id_room}");
+            Console.WriteLine($"Count     : {this.person_count}");
+            Console.WriteLine($"Details   : {this.details}");
+            Console.WriteLine($"Game name : {this.game_name}");
+        }
+
+
+
+    }
+}
