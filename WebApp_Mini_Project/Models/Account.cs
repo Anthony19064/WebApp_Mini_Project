a69b5088b248e3aa91719c0e04b603a2711035ac
Downueaa_ <162528777+Downuea2545@users.noreply.github.com>
2024-09-11 13:29:14 +0700

    Add Account model & Account Controller (simple)

---
+++ WebApp_Mini_Project/Models/Account.cs
@@ -0,0 +1,19 @@
+﻿namespace WebApp_Mini_Project.Models
+{
+    public class Account
+    {
+        public string Username { get; set; }
+        public string Password { get; set; } 
+
+        public string Email { get; set; }
+
+        public Account(string username, string password, string email)
+        {
+            Username = username;
+            Password = password;
+            Email = email;
+        }
+        
+       
+    }
+}
