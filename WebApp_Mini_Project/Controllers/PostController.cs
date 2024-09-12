9c2597179430006c9700327ecfbe58640ace3c5c
Anthony19064 <kawanichyota1@gmail.com>
2024-09-13 02:47:24 +0700

    Test font end

--- WebApp_Mini_Project/Controllers/PostController.cs
+++ WebApp_Mini_Project/Controllers/PostController.cs
@@ -14,6 +14,10 @@ namespace WebApp_Mini_Project.Controllers
             new Post("AJMCJ19", 5, "หาสาวเสียงน่ารักๆ", "Valorant"),
             new Post("1748200", 3, "หาควายให้แบกขอแรงค์ iron", "Rov"),
             new Post("xxxxxxx", 2, "Gold - Plat ชิลๆ ไม่ซี", "Valorant"),
+            new Post("AJMCJ19", 5, "หาสาวเสียงน่ารักๆ", "Valorant"),
+            new Post("1748200", 3, "หาควายให้แบกขอแรงค์ iron", "Rov"),
+            new Post("xxxxxxx", 2, "Gold - Plat ชิลๆ ไม่ซี", "Valorant"),
+
         };
 
         // GET: Post
