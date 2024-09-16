4de048d2ac249f0d5d16c31a5700646049586ba3
Anthony19064 <kawanichyota1@gmail.com>
2024-09-16 15:47:10 +0700

    Fix

---
+++ WebApp_Mini_Project/Models/AccountViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace WebApp_Mini_Project.Models
+{
+    public class AccountViewModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public IFormFile ProfilePictureFile { get; set; }
+    }
+}
