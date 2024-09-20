35c602928f7bd382930729eec0b5e0d6503436dd
RavenLace2 <pasagon166@gmail.com>
2024-09-20 12:19:21 +0700

    fix post

---
+++ WebApp_Mini_Project/Migrations/20240919125822_AddPostDB.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApp_Mini_Project.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPostDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Accounts",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProfilePicture = table.Column<byte[]>(type: "varbinary(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accounts", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Id_room = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name_room = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Count_person = table.Column<int>(type: "int", nullable: false),
+                    Max_person = table.Column<int>(type: "int", nullable: false),
+                    Details = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Game = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    User_Post = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    User_Picture = table.Column<byte[]>(type: "varbinary(max)", nullable: true),
+                    User_list = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Accounts");
+
+            migrationBuilder.DropTable(
+                name: "Posts");
+        }
+    }
+}
