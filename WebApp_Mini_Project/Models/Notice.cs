a3431d7ba1f43f3009f8bd30984c02b96eb66923
Downueaa_ <162528777+Downuea2545@users.noreply.github.com>
2024-09-29 07:19:03 +0700

    สร้าง Notice และเอา Obj ของ Notice ไปเก็บใน Account

---
+++ WebApp_Mini_Project/Models/Notice.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApp_Mini_Project.Models
+{
+    public class Notice
+    {
+        [Key]
+        public int ID { get; set; }
+
+        // ID Account
+        public int UserID { get; set; }
+
+        // Message
+        [Required]
+        public string Message { get; set; }
+
+        // Photo
+        public byte[]? Picture { get; set; }
+
+
+        // คุณสมบัติการนำทาง (ไม่บังคับ) เพื่อเชื่อมโยงกับโมเดลบัญชีผู้ใช้
+        [ForeignKey("UserID")]
+        public virtual Account User { get; set; }
+
+    }
+}
